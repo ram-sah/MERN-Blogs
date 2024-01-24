2770c7889a96f303fb9e09de409d16bb7421cd86
ram-sah <mohan2036@gmail.com>
2024-01-24 15:59:11 -0500

    added navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Headers from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Headers/>
+    <Routes >
+    <Route path='/' element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
