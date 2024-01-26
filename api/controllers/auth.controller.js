c144a07fa3cb436e77e38b3b9ac2f445c0486543
ram-sah <mohan2036@gmail.com>
2024-01-26 00:30:07 -0500

    added bcryptjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,35 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) =>{
-    console.log(req.body)
-}
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === "" || email === "") {
+        return res.status(400).json({ message: "Enter valid data in all fields" });
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    try {
+        // Check if the email already exists
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            // Email is already in use
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
+        // Save the new user
+        await newUser.save();
+        res.json( "Sign up successful" );
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
