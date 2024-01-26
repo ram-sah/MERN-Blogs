8484fcf643a3db77b89a21dd9b1a945e70eee8f6
ram-sah <mohan2036@gmail.com>
2024-01-25 23:00:21 -0500

    created auth controller

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from '../routes/user.route.js'
-
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO)
     .then(() => {
@@ -14,8 +15,11 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
 });
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
