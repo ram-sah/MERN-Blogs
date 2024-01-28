4963a24c9b5942fb2291600b3cc4e401851f856c
ram-sah <mohan2036@gmail.com>
2024-01-28 18:41:32 -0500

    Add signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from 'flowbite-react';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+    console.log(formData)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Each fields must be fill out !");
+    }
+    try {
+      setErrorMessage(null);
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(formData),
+        });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
   return (
     <>
-      <div className="min-h-screen mt-20">
+      <div className="min-h-screen mt-20 ">
         <div className="main flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
           <div className="left flex-1">
             <Link
               to="/"
               className=" dark:text-white font-bold text-4xl"
             >
-              <span className="px-3 py-2 bg-gradient-to-r from-yellow-500 via-purple-400 to-blue-400 rounded-lg text-white">
+              <span className="px-8 py-2 bg-gradient-to-r from-blue-500 via-purple-400 to-blue-400 rounded-lg text-white">
                 Ram's
               </span>
               Blogs
             </Link>
-            <p className="text-sm mt-5">This is blogs project. Signup with username and password or with google account</p>
+            <div className="text-sm mt-6" ><h1 className=" text-2xl text-blue-600 font-thin tracking-wider">Publish your passions, your way</h1>
+              <p className="my-1">Signup with your email or with google account.</p></div>
           </div>
           <div className="right flex-1">
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
               <div className="">
                 <Label value="Your username" />
                 <TextInput
                   type="text"
                   placeholder="username"
-                  id="username"
+                  id="username" onChange={handleChange}
                 />
               </div>
               <div className="">
@@ -34,7 +72,7 @@ const SignUp = () => {
                 <TextInput
                   type="text"
                   placeholder="email@email.com"
-                  id="email"
+                  id="email" onChange={handleChange}
                 />
               </div>
               <div className="">
@@ -42,17 +80,31 @@ const SignUp = () => {
                 <TextInput
                   type="password"
                   placeholder="password"
-                  id="password"
+                  id="password" onChange={handleChange}
                 />
               </div>
-              <Button gradientDuoTone='purpleToBlue' type="submit" >
-                Sign Up
+              <Button gradientDuoTone='purpleToBlue' type="submit" disabled= {loading}>
+                {
+                  loading ? (
+                    <>
+                    <Spinner size="sm" />
+                    <span className="pl-5">Loading...</span>
+                    </>
+                  ) : ('Sign Up')
+                }
               </Button>
             </form>
             <div className="my-2 text-sm gap-2 flex">
-            <span >Have an account ?</span>
-            <Link to='/' className="text-blue-500"> Sign In</Link>
+              <span >Have an account ?</span>
+              <Link to='/' className="text-blue-500"> Sign In</Link>
             </div>
+            {
+              errorMessage && (
+                <Alert className="mt-4" color="failure">
+                  {errorMessage}
+                </Alert>
+              )
+            }
           </div>
         </div>
       </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      }
+    },
+  },
   plugins: [react()],
-})
+});
