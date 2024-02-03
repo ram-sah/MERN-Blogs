f92fb32a5700d0f218457f2b57ccb9ff7420f37e
ram-sah <mohan2036@gmail.com>
2024-02-03 09:07:02 -0500

    made dashboard route private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Headers from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import FooterComponent from './components/FooterComponent';
+import PrivateRoute from './components/PrivateRoute';
 
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Headers from './components/Header'
-import Home from './pages/Home'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import FooterComponent from './components/FooterComponent'
 const App = () => {
   return (
     <BrowserRouter>
-    <Headers/>
-    <Routes >
-    <Route path='/' element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/projects" element={<Projects />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    </Routes>
-    <FooterComponent />
-    </BrowserRouter>
-  )
-}
+      <Headers />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
 
-export default App
+        {/* Using PrivateRoute to protect the dashboard route */}
+        <Route path="/dashboard/*" element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>}
+        />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      <FooterComponent />
+    </BrowserRouter>
+  );
+};
+
+export default App;
