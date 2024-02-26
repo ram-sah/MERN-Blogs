f0ce1f0fe1b3029b44ab822c987922e81763c1a4
ram-sah <mohan2036@gmail.com>
2024-02-26 02:41:16 -0500

    correction

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Pojects</h1>
-      <p className='text-md text-gray-500 dark:text-gray-300 '>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <p className='text-md text-gray-500 dark:text-gray-300 '>Enjoy with multiple choice questions and answers of HTML, CSS, Reactjs, NodeJs, Jquery and JavaScript!</p>
       <CallToAction />
     </div>
   )
