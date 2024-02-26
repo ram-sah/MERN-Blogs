224e04d33e5f1e33f5dd3a7e0ec343cd9a168f25
ram-sah <mohan2036@gmail.com>
2024-02-25 23:55:29 -0500

    Added about & projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,47 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Ram's MyBlogs!
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6 '>
+            <p>
+              At MyBlogs, we're dedicated to the world of web development and coding.
+              Our platform is a hub for developers, designers, and tech enthusiasts—a place where
+              innovation thrives, knowledge is shared, and communities flourish.
+            </p>
 
+            <p>
+              Our Mission: <br/>
+
+              🌟 To provide a platform for web developers of all levels to share their insights, tutorials, and experiences with fellow enthusiasts.<br/>
+
+              🌟 To foster a collaborative and supportive community where developers can learn from each other, exchange ideas, and stay updated on the latest trends and technologies in the ever-evolving world of web development.<br/>
+
+              🌟 To inspire and empower individuals to pursue their passion for coding, unlock their full potential, and contribute to the advancement of technology and digital innovation.
+            </p>
+
+            <p>
+              Our Vision:<br/>
+
+              🌿 We envision a future where coding is accessible to all, diversity is celebrated, and technology is used to create positive change in the world.<br/>
+
+              🌿 We strive to be a trusted resource and a source of inspiration for aspiring developers, offering guidance, mentorship, and opportunities for growth.<br/>
+
+              🌿 We are committed to promoting inclusivity, diversity, and equality in the tech industry, striving to create a more inclusive and representative community where everyone feels welcome and valued.
+            </p>
+            <p>
+              Whether you're a seasoned developer, a coding enthusiast, or simply curious about the world of web development, we invite you to join us on this journey of learning, collaboration, and innovation. Together, let's push the boundaries of what's possible, transform ideas into reality, and make a meaningful impact through the power of code.
+
+              Thank you for being a part of our community. Together, let's build a brighter future through technology and coding.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default About
